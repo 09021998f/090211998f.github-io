4763e7d1f491a0b3cbe7ad517386a089db3a793b
09021998f <ariasfacu1998@gmail.com>
2024-06-14 19:29:32 -0300

    Subiendo la pagina

---
+++ js/script_formulario.js
@@ -0,0 +1,64 @@
+
+
+function validar_datos(){
+    const NOMBRES = ['carlos', 'susana']
+    const DNI = ['123', '321']
+    
+    let nombre = document.getElementById('nombre').value;
+    let dni = document.getElementById('dni').value;
+    let alerta = document.getElementById('alerta');
+    if ((nombre === '') || (dni == '')){
+        alerta.textContent = 'Debe completar todos los campos';
+        alerta.style.color = 'red'
+        
+    } else {
+
+        if (NOMBRES.includes(nombre) && DNI.includes(dni)){
+            alerta.textContent = 'Usted ya a solicitado su plan';
+            alerta.style.color = 'red'
+        } else{
+            alerta.textContent = 'Solicitud realizada con exito!';
+            alerta.style.color = '#32CD32'
+        }
+    }
+
+    
+}
+
+function validar_datos_inicio(){
+    const USUARIO = ['admin']
+    const PASSWORD = ['123']
+    
+    let user = document.getElementById('username').value;
+    let pass = document.getElementById('password').value;
+    let alert = document.getElementById('alert');
+    console.log(user, pass)
+    if (USUARIO.includes(user) && PASSWORD.includes(pass)){
+        window.location.href = '../home_tecnico.html';
+    } else{
+        alert.textContent = 'Usuario o Contraseña incorrectos.';
+    }
+
+}
+
+
+
+
+
+// document.getElementById('formulario').addEventListener('submit', function (even) {
+//     const NOMBRES = ['carlos', 'susana']
+//     const DNI = ['123', '321']
+    
+//     let name = document.getElementById('dni').value;
+//     let dni = document.getElementById('nombre').value;
+//     let alerta = document.getElementById('alerta');
+
+
+//     if (NOMBRES.includes(name) && DNI.includes(dni)){
+//         alerta.textContent = '${name} Usted ya a solicitado su plan';
+//     } else{
+//         alerta.textContent = 'Solicitud realizada con exito!';
+//     }
+
+
+// })
